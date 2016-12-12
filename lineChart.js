4c7bc990d55ca14d038b5e7f182e0e34a6792b0e
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-12-12 15:39:03 +0530

    lineChart

--- lineChart.js
+++ lineChart.js
@@ -1,16 +1,17 @@
-var w = 100;
+var w = 500;
 var h = 200;
 
 monthlySales = [
     {"month":10, "sales":10},
-    {"month":20, "sales":70},
+    {"month":20, "sales":20},
     {"month":30, "sales":64},
     {"month":40, "sales":51},
     {"month":50, "sales":45},
-    {"month":60, "sales":30},
-    {"month":70, "sales":16},
-    {"month":80, "sales":23},
-    {"month":90, "sales":50}
+    {"month":60, "sales":70},
+    {"month":70, "sales":36},
+    {"month":80, "sales":33},
+    {"month":90, "sales":50},
+    {"month":100, "sales":100}
     
 ];
 
@@ -20,7 +21,7 @@ var svg = d3.select("body").append("svg").attr("width",w).attr("height",h);
 
 var lineFun = d3.line()
 .x(function(d){return d.month*2;})
-.y(function(d){return d.sales;});
+.y(function(d){return h-d.sales;});
 
    
 var viz = svg.append("path")
