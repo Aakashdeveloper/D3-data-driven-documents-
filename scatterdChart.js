59e7016bc35703260be309c0a0a84422c48780b1
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-12-13 13:00:15 +0530

    a

--- scatterdChart.js
+++ scatterdChart.js
@@ -19,15 +19,44 @@ function salesKPI(d){
   if(d>=50){return "#33CC66"}
   else if(d<=50){return "#666666"}
 }
-
+function showMinMax(ds,col,val,type){
+    var max = d3.max(ds,function(d){return d[col];});
+    var min = d3.min(ds,function(d){return d[col];});
+    
+    if(type=='minmax' && (val ==max || val == min)){
+	return val;
+    }else if(type =='all'){
+	return val;
+    }
+}
 var svg = d3.select("body").append("svg").attr("width",w).attr("height",h);
 
 
 var dots = svg.selectAll("circle")
+    .data(monthlySales)
+    .enter()
+    .append("circle")
+    .attr("cx",function(d){return d.month*3})
+    .attr("cy",function(d){return h-d.sales})
+    .attr("r","5")
+    .attr("fill",function(d){return salesKPI(d.sales)})
+
+
+var dots = svg.selectAll("text")
      .data(monthlySales)
-.enter()
-.append("circle")
-.attr("cx",function(d){return d.month*3})
-.attr("cy",function(d){return h-d.sales})
-.attr("r","5")
-.attr("fill",function(d){return salesKPI(d.sales)})
+     .enter()
+     .append("text")
+     .text(function(d){return showMinMax(monthlySales,'sales',d.sales,'minmax')})
+     .attr("x",function(d){return (d.month*3)-25;})
+     .attr("y",function(d){return h-d.sales;})
+     .attr("font-size","12px")
+     .attr("fill","#666666")
+     .attr("text-anchor","start")
+     
+     
+     
+     
+     
+     
+     
+     
